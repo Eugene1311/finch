0ed3b0ef64c97c7b48ac56620211ab3dd90c5ec9
Eugene1311 <ef7@mail.ru>
2014-12-16 16:43:05 +0300

    1

--- js/script.js
+++ js/script.js
@@ -5,6 +5,22 @@ $(function () {
 		var attr = $(this).attr('data-tab');
 		$('.slide').fadeOut(1200).removeClass('active-slide');
 		$('.slider').find('[data-tab="'+attr+'"]').fadeIn(1200).addClass('active-slide');
-		
 	});
+	$(window).scroll(function() {
+ 		if($(this).scrollTop() > 500) {
+		$('#toTop').fadeIn(1000);
+		} else {
+ 		$('#toTop').fadeOut(600);
+ 		}
+ 	});
+ 	$('#toTop').click(function() {
+ 		$('body,html').animate({scrollTop:0},800);
+ 	});
+//	$('#banner-slide').bjqs({
+//            animtype      : 'slide',
+//            height        : 320,
+//            width         : 3000 ,
+//            responsive    : true,
+//            randomstart   : true
+//    });
 });
